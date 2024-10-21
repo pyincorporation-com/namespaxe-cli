de196d59fd9b16044416f3a73ec1b0dc18d8beb9
gabriel nzilantuzu <gabrielnzilantuzu@gmail.com>
2024-10-22 00:24:46 +0300

    added all files

--- namespaxe/setup.py
+++ namespaxe/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='namespaxe',
-    version='0.1.0',
+    version='0.1.1',
     author='Gabriel Nzilantuzu',
     author_email='gabrielnzilantuzu@pyincorporation.com',
     description='A command-line tool for interacting with cloud services.',
